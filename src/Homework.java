59a0b4da10ca170a7005d0bda5b238efe6b2ff5a
CosmoBlack <soulker_mozyr@yahoo.com>
2023-01-26 11:49:10 +0200

    Task 1

--- src/Homework.java
+++ src/Homework.java
@@ -2,6 +2,20 @@ public class Homework {
 
     public static void main(String[] args) {
 
+        task1();
+
+    }
+
+    private static void task1() {
+
+        System.out.println("Задача 1");
+
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+
+        System.out.println("ФИО сотрудника — " + fullName);
 
     }
 
